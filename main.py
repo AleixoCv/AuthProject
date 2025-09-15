9de5ed1159fb6c802c8aa1ca1907d69e41e67971
AleixoCv <caleixodavi1811@gmail.com>
2025-09-15 19:00:22 -0300

    Adição de um front simples

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from auth import get_current_user
 from models import User
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.post("/auth/me")
 def auth_me(user: User = Depends(get_current_user)):
@@ -10,3 +16,7 @@ def auth_me(user: User = Depends(get_current_user)):
         "user": user,
         "ping": "Pong"
     }
+
+@app.get("/login", response_class=HTMLResponse)
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
